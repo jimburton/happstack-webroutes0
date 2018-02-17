0145bf3d2199f5449a41604ece56b606b512ddb9
Jim Burton <j.burton@brighton.ac.uk>
2018-02-17 14:46:23 +0000

    tidying up webservice

--- src/WebService/Main.hs
+++ src/WebService/Main.hs
@@ -1,28 +1,22 @@
 {-# LANGUAGE OverloadedStrings, FlexibleContexts, TypeFamilies, QuasiQuotes, TemplateHaskell, DeriveGeneric #-}
 module Main where
 
-import Prelude hiding    (reverse, show)
-import Control.Monad     (liftM, msum)
-import Control.Monad.IO.Class (liftIO)
-import Data.List         (sort)
-import Data.Text         (Text, pack, unpack, reverse, toUpper)
-import Happstack.Server
-import Text.Hamlet
-import Text.Lucius
-import Web.Routes
-import Web.Routes.Happstack
-import Web.Routes.TH
+import           Control.Monad.IO.Class (liftIO)
+import           Data.List         (intercalate)
+import           Data.Text         (Text)
+import           Happstack.Server
+import           Web.Routes
+import           Web.Routes.Happstack
+import           Web.Routes.TH
 import           Database.SQLite.Simple
 import           Database.SQLite.Simple.FromRow
-import Data.Dates
-import qualified Data.Text as T
-import qualified Prelude as P
-import Data.Aeson
-import GHC.Generics (Generic )
-import qualified Data.ByteString.Lazy as LB
+import           Data.Dates
+import           Data.Aeson
+import           GHC.Generics                  (Generic)
 import qualified Data.ByteString.Lazy.Char8 as BC
 
-data WeatherField = WeatherField {date :: T.Text, temperature :: Float} deriving (Generic, Eq, Show)
+data WeatherField = WeatherField {date :: Text, temperature :: Float}
+                    deriving (Generic)
 
 instance FromRow WeatherField where
   fromRow = WeatherField <$> field <*> field 
@@ -33,49 +27,23 @@ instance ToRow WeatherField where
 instance ToJSON   WeatherField
 instance FromJSON WeatherField
 
-show :: Show a => a -> Text
-show = pack . P.show
-
-data Sitemap
-    = Home
-    | Date Text
-    | Range Text Text
+data Sitemap = Date Text
+             | Range Text Text
 
 $(derivePathInfo ''Sitemap)
 
 siteRoute :: Connection -> Sitemap -> RouteT Sitemap (ServerPartT IO) Response
 siteRoute conn url = 
     case url of
-        Home             -> appRoot
-        (Date d)         -> dayHandler d conn 
-        (Range d1 d2)    -> rangeHandler d1 d2 conn 
-
-convRender :: (url -> [(Text, Maybe Text)] -> Text)
-           -> (url -> [(Text, Text)]-> Text)
-convRender maybeF url params = maybeF url $ map (\(t1, t2) -> (t1, Just t2)) params
-
-renderFunction :: MonadRoute m => m (URL m -> [(Text, Text)] -> Text)
-renderFunction = fmap convRender askRouteFn
-
-appRoot = do
-    urlF <- renderFunction
-    ok $ toResponse $ ([hamlet|
-$doctype 5
-<html>
-  <head>
-  <body>
-    Small demo webservice using Happstack, web-routes and Julius.
-    Find the weather for a given date <a href=@{Date "2017-01-01"}> here.
-    Find the weather between two dates <a href=@{Range "2017-01-01" "2017-12-31"}> here.
-    |]) urlF
+        (Date d)      -> dayHandler d conn 
+        (Range d1 d2) -> rangeHandler d1 d2 conn 
 
 dayHandler :: Text -> Connection -> RouteT Sitemap (ServerPartT IO) Response
 dayHandler d conn = do
-  r <- liftIO $ (queryNamed conn "SELECT the_date, temperature \
-                                \ FROM weather \
-                                \ WHERE the_date = :dt" [":dt" := d] :: IO [WeatherField])
-  let res = if null r then "NO DATA" else BC.unpack $ Data.Aeson.encode $ head r
-  ok $ toResponse res
+  r <- liftIO (queryNamed conn "SELECT the_date, temperature \
+                               \ FROM weather \
+                               \ WHERE the_date = :dt" [":dt" := d] :: IO [WeatherField])
+  ok $ toResponse (listToOutput r)
 
 rangeHandler :: Text -> Text -> Connection -> RouteT Sitemap (ServerPartT IO) Response
 rangeHandler d1 d2 conn = do
@@ -84,14 +52,17 @@ rangeHandler d1 d2 conn = do
                               \ WHERE the_date >= :d1 \
                               \ AND the_date <= :d2"
        [":d1" := d1, ":d2" := d2] :: IO [WeatherField])
-  let res = if null r then "NO DATA" else concat (map (BC.unpack . Data.Aeson.encode) r)
-  ok $ toResponse res
+  ok $ toResponse (listToOutput r)
+
+listToOutput :: ToJSON a => [a] -> String
+listToOutput xs = "[" ++ intercalate "," (map (BC.unpack . encode) xs) ++ "]"
     
 sitemapSite :: Connection -> Site Sitemap (ServerPartT IO Response)
-sitemapSite conn = setDefault Home $ mkSitePI (runRouteT (siteRoute conn)) 
+sitemapSite conn = mkSitePI (runRouteT (siteRoute conn)) 
 
 main :: IO()
 main = do
     conn <- open "data/np-weather.db"
-    simpleHTTP nullConf $ implSite "http://localhost:8000" "/weather" (sitemapSite conn)
+    simpleHTTP nullConf $
+      implSite "http://localhost:8000" "/weather" (sitemapSite conn)
       
