138541c3165cf0eb039a7bfe60336a2704184ace
Jim Burton <j.burton@brighton.ac.uk>
2018-02-17 16:20:12 +0000

    Added logging

--- src/WebService/Main.hs
+++ src/WebService/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import           System.Log.Logger ( updateGlobalLogger
                                    , rootLoggerName
                                    , setLevel
+                                   , debugM
                                    , Priority(..)
                                    )
 import           Control.Monad.IO.Class (liftIO)
@@ -22,21 +23,21 @@ import qualified Data.ByteString.Lazy.Char8 as BC
 data WeatherField = WeatherField {date :: Text, temperature :: Float}
                     deriving (Generic)
 
-instance FromRow WeatherField where
+instance FromRow WeatherField where -- ^ Marshal data from DB to our ADT
   fromRow = WeatherField <$> field <*> field 
 
-instance ToRow WeatherField where
+instance ToRow WeatherField where -- ^ Marshal data from our ADT to the DB
   toRow (WeatherField theDate temp) = toRow (theDate, temp)
 
-instance ToJSON   WeatherField
-instance FromJSON WeatherField
+instance ToJSON   WeatherField -- ^ Marshal data from our ADT to JSON
+instance FromJSON WeatherField -- ^ Marshal data from JSON to our ADT
 
-data Sitemap = Date Text
+data Sitemap = Date Text       -- ^ The endpoints in the webservice
              | Range Text Text
 
-$(derivePathInfo ''Sitemap)
+$(derivePathInfo ''Sitemap)    -- ^ Turn our ADT into a set of web routes
 
-{-| Translate a URL into a Response. -}
+{-| Translate an endpoint into a Response. -}
 siteRoute :: Connection -> Sitemap -> RouteT Sitemap (ServerPartT IO) Response
 siteRoute conn url = 
     case url of
@@ -49,6 +50,7 @@ dayHandler d conn = do
   r <- liftIO (queryNamed conn "SELECT the_date, temperature \
                                \ FROM  weather \
                                \ WHERE the_date = :dt" [":dt" := d] :: IO [WeatherField])
+  liftIO $ debugM "Date Query" (listToOutput r)
   ok $ toResponse (listToOutput r)
 
 {-| Handle requests for a date range. -}
@@ -59,6 +61,7 @@ rangeHandler d1 d2 conn = do
                               \ WHERE  the_date >= :d1 \
                               \ AND    the_date <= :d2"
        [":d1" := d1, ":d2" := d2] :: IO [WeatherField])
+  liftIO $ debugM "Range Query" (listToOutput r)
   ok $ toResponse (listToOutput r)
 
 {-| Turn a list of WeatherFields into a JSON object. -}
@@ -73,7 +76,7 @@ sitemapSite conn = mkSitePI (runRouteT (siteRoute conn))
 routing function. -}
 main :: IO()
 main = do
-  updateGlobalLogger rootLoggerName (setLevel INFO)
+  updateGlobalLogger rootLoggerName (setLevel INFO) -- change level to DEBUG for testing
   conn <- open "data/np-weather.db"
   simpleHTTP nullConf $
       implSite "http://localhost:8000" "/weather" (sitemapSite conn)
