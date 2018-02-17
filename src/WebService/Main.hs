22d1a0344ffbec2eaae40d105aac43e516d9068a
Jim Burton <j.burton@brighton.ac.uk>
2018-02-17 13:17:36 +0000

    tidying up

--- src/WebService/Main.hs
+++ src/WebService/Main.hs
@@ -40,7 +40,6 @@ data Sitemap
     = Home
     | Date Text
     | Range Text Text
-    | Plus Int Int
 
 $(derivePathInfo ''Sitemap)
 
@@ -50,29 +49,6 @@ siteRoute conn url =
         Home             -> appRoot
         (Date d)         -> dayHandler d conn 
         (Range d1 d2)    -> rangeHandler d1 d2 conn 
-        (Plus i1 i2)     -> plusHandler i1 i2
-
-siteLayout :: HtmlUrl Sitemap -> HtmlUrl Sitemap
-siteLayout body = [hamlet|
-$doctype 5
-<html>
-  <head>
-    <link href="/site.css" rel="stylesheet" media="all" type="text/css">
-  <body>
-    <div class="container">
-      ^{body}
-      <p><a href=@{Home}>Home</a>
-|]
-
-stylesheet = [lucius|
-body {
-    background: #EBEBEB;
-    height: 100%;
-}
-.container {
-    padding: 20px;
-}
-|] undefined -- no URL rendering in this css
 
 convRender :: (url -> [(Text, Maybe Text)] -> Text)
            -> (url -> [(Text, Text)]-> Text)
@@ -83,39 +59,37 @@ renderFunction = fmap convRender askRouteFn
 
 appRoot = do
     urlF <- renderFunction
-    ok $ toResponse $ siteLayout ([hamlet|
-    <p>Small demo application for Happstack, web-routes, Hamlet and Lucius.
-    <p>Add two numbers by going to /plus/int1/int2, e.g. 
-        <a href=@{Plus 13 37}> here.
-    <p>Also other boring stuff.
-    |]) urlF
-
-plusHandler n1 n2 = do 
-    urlF <- renderFunction
-    ok $ toResponse $ siteLayout ([hamlet|
-    <p>Input numbers are #{n1} and #{n2}
-    <p>Sum is: #{n1 + n2}
+    ok $ toResponse $ ([hamlet|
+$doctype 5
+<html>
+  <head>
+  <body>
+    Small demo webservice using Happstack, web-routes and Julius.
+    Find the weather for a given date <a href=@{Date "2017-01-01"}> here.
+    Find the weather between two dates <a href=@{Range "2017-01-01" "2017-12-31"}> here.
     |]) urlF
 
 dayHandler :: Text -> Connection -> RouteT Sitemap (ServerPartT IO) Response
 dayHandler d conn = do
-  liftIO $ putStrLn ("Looking for: " ++ P.show d)
   urlF <- renderFunction
-  r <- liftIO $ (queryNamed conn "SELECT the_date, temperature FROM weather WHERE the_date = :dt" [":dt" := d] :: IO [WeatherField])
-  liftIO (putStrLn (BC.unpack $ Data.Aeson.encode r))
+  r <- liftIO $ (queryNamed conn "SELECT the_date, temperature \
+                                \ FROM weather \
+                                \ WHERE the_date = :dt" [":dt" := d] :: IO [WeatherField])
   let res = if null r then "NO DATA" else BC.unpack $ Data.Aeson.encode $ head r
-  ok $ toResponse $ siteLayout ([hamlet|
+  ok $ toResponse $ ([hamlet|
     #{res}
     |]) urlF
 
 rangeHandler :: Text -> Text -> Connection -> RouteT Sitemap (ServerPartT IO) Response
 rangeHandler d1 d2 conn = do
-  liftIO $ putStrLn ("Looking for: " ++ P.show d1 ++ "/" ++ P.show d2)
   urlF <- renderFunction
-  r <- liftIO (queryNamed conn "SELECT the_date, temperature FROM weather WHERE the_date >= :d1 AND the_date <= :d2"
+  r <- liftIO (queryNamed conn "SELECT the_date, temperature \
+                              \ FROM weather \
+                              \ WHERE the_date >= :d1 \
+                              \ AND the_date <= :d2"
        [":d1" := d1, ":d2" := d2] :: IO [WeatherField])
   let res = if null r then "NO DATA" else concat (map (BC.unpack . Data.Aeson.encode) r)
-  ok $ toResponse $ siteLayout ([hamlet|
+  ok $ toResponse $ ([hamlet|
     #{res}
     |]) urlF
     
@@ -125,12 +99,5 @@ sitemapSite conn = setDefault Home $ mkSitePI (runRouteT (siteRoute conn))
 main :: IO()
 main = do
     conn <- open "data/np-weather.db"
-    r <- query_ conn "SELECT the_date, temperature FROM weather" :: IO [WeatherField]
-    mapM_ (\x -> putStrLn $ P.show x) r
-    simpleHTTP nullConf $ msum [
-      do dirs "site.css" nullDir
-         setHeaderM "Content-Type" "text/css"
-         ok $ toResponse $ renderCss stylesheet
-      , implSite "http://localhost:8000" "/site" (sitemapSite conn)
-      , seeOther ("/site" :: String) (toResponse ())
-      ]
+    simpleHTTP nullConf $ implSite "http://localhost:8000" "/weather" (sitemapSite conn)
+      
